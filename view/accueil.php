a2e7cdb795d484a179b36798ab10dd1eb487b657
Max Reis <maxreis507@gmail.com>
2025-09-29 12:35:30 +0200

    feat: fix accueil

--- view/accueil.php
+++ view/accueil.php
@@ -1,2 +1,11 @@
 <h1>Accueil</h1>
 
+<h2>Dernier produit</h2>
+
+<h3><?= $lastProduct['name']?></h3>
+
+<div>
+    <p><?= $lastProduct['description']?></p>
+    <p><?= $lastProduct['price']?></p>
+    <img src='<?= $lastProduct['image']?>' style="width:300px" alt="">
+</div>
